bbe92a31d2a3c5f66bf0cdeb2a809256194f98f8
sumitgupta1225 <sumitgupta1225@gmail.com>
2020-05-27 12:14:01 +0530

    Added JoinUs controller

---
+++ Controllers/JoinUsController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NETCoreDemo.Controllers
+{
+    public class JoinUsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
