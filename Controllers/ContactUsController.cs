3d1cf4ad68125610ea1fa378a2668bbf7f9b8457
sumitgupta1225 <sumitgupta1225@gmail.com>
2020-05-27 11:46:32 +0530

    Added ContactUs controller

---
+++ Controllers/ContactUsController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NETCoreDemo.Controllers
+{
+    public class ContactUsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
