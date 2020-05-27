c656bcf2a14ab073f9ad8f70e5287c3a0be5c74f
sumitgupta1225 <sumitgupta1225@gmail.com>
2020-05-27 11:22:08 +0530

    initial commit

---
+++ Controllers/OfflineController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.Collections;
+using System.Configuration;
+namespace ASP.NETCoreDemo.Controllers
+{
+    public class OfflineController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
