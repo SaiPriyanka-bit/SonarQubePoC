c603979fcc7c080c0c6ab6fc9cedb60bdf3c6714
sumitgupta1225 <sumitgupta1225@gmail.com>
2020-05-27 12:25:10 +0530

    added default controller

--- Controllers/DefaultController.cs
+++ Controllers/DefaultController.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ASP.NETCoreDemo.Controllers
 {
-    public class JoinUsController : Controller
+    public class DefaultController : Controller
     {
         public IActionResult Index()
         {
